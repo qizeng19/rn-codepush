aba4b47fb74bed3fca565011527b4e717e0371cb
qizeng <136800645@qq.com>
2023-07-04 15:17:23 +0800

    init

---
+++ App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+function App(): JSX.Element {
+
+  const backgroundStyle = {
+    backgroundColor: "red",
+  };
+
+  return (
+    <View style={backgroundStyle}>
+      <Text>124</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
